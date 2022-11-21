71baea8836b565c86a10fd1e895b567acd1be70e
Mathieu Jeanmougin <m.jeanmougin@tadaweb.com>
2022-11-21 16:59:46 +0100

    feat(searchfield): make the searchfield component stateful

--- src/NavBar/SearchField.tsx
+++ src/NavBar/SearchField.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchField: React.FunctionComponent = () => {
-  return <input className="search-field" type="text" placeholder="Search" />;
+  const [inputValue, setInputValue] = useState("");
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <input
+      value={inputValue}
+      onChange={onChange}
+      className="search-field"
+      type="text"
+      placeholder="Search"
+    />
+  );
 };
 
 export { SearchField };
