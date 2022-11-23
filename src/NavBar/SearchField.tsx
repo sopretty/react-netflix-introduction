8e364dd47ea48e5139e799dc9ae68aa8da4b8fee
Mathieu Jeanmougin <m.jeanmougin@tadaweb.com>
2022-11-23 18:39:41 +0100

    fix(step-04): instructions

--- src/NavBar/SearchField.tsx
+++ src/NavBar/SearchField.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const SearchField: React.FunctionComponent = () => {
-  return <input className="search-field" type="text" placeholder="Search" />;
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // changer la valeur de l'état local 
+    // vous récupérez la valeur de ce que tape l'utilisateur via event.target.value
+  };
+
+  return (
+    <input
+      onChange={onChange}
+      className="search-field"
+      type="text"
+      placeholder="Search"
+    />
+  );
 };
 
 export { SearchField };
