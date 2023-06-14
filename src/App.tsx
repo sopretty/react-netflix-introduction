e4cc0a659809a70be20ac77381b19db61e94c92b
Mathieu Jeanmougin <m.jeanmougin@tadaweb.com>
2023-06-14 19:34:36 +0200

    fix(props)

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App: React.FunctionComponent = () => {
       <div className="cards">
         <Card
           title="The Walking Dead"
-          srcImage="https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:376/h:210/q:75/https://bleedingcool.com/wp-content/uploads/2022/09/TWD11_KA_HZ-2.jpg"
+          imageSrc="https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:376/h:210/q:75/https://bleedingcool.com/wp-content/uploads/2022/09/TWD11_KA_HZ-2.jpg"
         />
         <CardWithOptionalProps />
       </div>
