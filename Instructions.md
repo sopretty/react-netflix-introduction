a7a249892aa9ee04826a1eb6f149779f85fed735
Mathieu Jeanmougin <m.jeanmougin@tadaweb.com>
2023-06-14 18:17:38 +0200

    fix(instructions)

--- Instructions.md
+++ Instructions.md
@@ -1,6 +1,21 @@
 1. Modifiez le composant « Card.tsx »
 
 - Ajoutez des propriétés les propriétés « title » et « imageSrc » au composant « Card »
+- [Typescript]: ces deux propriétés sont de type « string »
 - Modifiez le JSX pour que ces propriétés soient prises en compte pour créer votre « Card »
+- [Aide]: exemple de JSX pour le composant Card.tsx
+  <div className="card">
+      <span style={{ color: "#e5e5e5" }}>{title}</span>
+      <img
+        style={{ borderRadius: "0.2vw" }}
+        src={imageSrc}
+        alt=""
+        width="300"
+        height="200"
+      />
+    </div>`
 
 2. Modifiez le JSX du composant « App.tsx » pour ajouter les propriétés désirées
+  
+- Les propriétés passées peuvent être écrites directement dans le JSX
+- Pour la propriété « imageSrc », n’hésitez pas à trouver en ligne une url d’image de votre film ou de votre série préférée
--- src/Card/CardOptionalProps.tsx
+++ src/Card/CardOptionalProps.tsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const CardWithOptionalProps: React.FunctionComponent<{
   title?: string;
-  srcImage?: string;
+  imageSrc?: string;
 }> = ({
   title = "Breaking Bad",
-  srcImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_Xz7AhXy8_BzMJfMTSOIrPPhbprot1bg3_A&usqp=CAU",
+  imageSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_Xz7AhXy8_BzMJfMTSOIrPPhbprot1bg3_A&usqp=CAU",
 }) => {
   return (
     <div className="card">
-      <span style={{ color: "#e5e5e5" }}>{title}</span>
-      <img
-        style={{ borderRadius: "0.2vw" }}
-        src={srcImage}
-        alt=""
-        width="300"
-        height="200"
-      />
-    </div>
+    <span style={{ color: "#e5e5e5" }}>{title}</span>
+    <img
+      style={{ borderRadius: "0.2vw" }}
+      src={imageSrc}
+      alt={title}
+      width="300"
+      height="200"
+    />
+  </div>
   );
 };
 
